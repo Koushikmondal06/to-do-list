101af3ab4c283b607315af1caaa2db982d1511a0
Koushik Mondal <Koushikmondal0069@gmail.com>
2025-01-29 15:13:00 +0530

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import { v4 as uuidv4 } from 'uuid'
 import { FaEdit } from "react-icons/fa";
 import { RiDeleteBin6Line } from "react-icons/ri";
